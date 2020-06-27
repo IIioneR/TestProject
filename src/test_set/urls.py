9e4eb3e3869234a4f240b78a126fbceecc4817c6
IIioneR <IIioneR@ukr.net>
2020-06-27 13:10:40 +0300

    done 19hm, flake8

--- src/test_set/urls.py
+++ src/test_set/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from test_set.views import TestListView, TestRunView
+from test_set.views import TestListView, TestRunView, StartTestView
 
 app_name = 'tests'
 
 urlpatterns = [
     path('', TestListView.as_view(), name='list'),
     path('<int:pk>/question/<int:seq_nr>', TestRunView.as_view(), name='testrun_step'),
+    path('<int:pk>/start', StartTestView.as_view(), name='start'),
     # path('<int:pk>/start', StartTestView.as_view(), name='start'),
 ]
--- src/test_set/views.py
+++ src/test_set/views.py
@@ -1,12 +1,16 @@
+import datetime
+
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
+from django.db.models import Max
 from django.http import HttpResponse
-from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy, reverse
 from django.utils.http import urlencode
 from django.views import View
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
-from test_set.models import Test, Question
+from test_set.models import Test, Question, TestResult, Answer, TestResultDetail
 
 
 class TestListView(LoginRequiredMixin, ListView):
@@ -26,18 +30,99 @@ class TestListView(LoginRequiredMixin, ListView):
 
 
 class TestRunView(View):
+    PREFIX = 'answer_'
 
     def get(self, request, pk, seq_nr):
         question = Question.objects.filter(test__id=pk, number=seq_nr).first()
 
-        answers = [answer.text for answer in question.answers.all()]
-        return render(request=request,
-                      template_name='testrun.html',
-                      context={
-                          'question': question,
-                          'answers': answers
-                      }
-                      )
+        answers = [
+            answer.text
+            for answer in question.answers.all()
+        ]
+
+        return render(
+            request=request,
+            template_name='testrun.html',
+            context={
+                'question': question,
+                'answers': answers,
+                'prefix': self.PREFIX
+            }
+        )
 
     def post(self, request, pk, seq_nr):
-        return HttpResponse("OK")
+        test = Test.objects.get(pk=pk)
+        question = Question.objects.filter(test__id=pk, number=seq_nr).first()
+
+        answers = Answer.objects.filter(
+            question=question
+        ).all()
+
+        choices = {
+            k.replace(self.PREFIX, ''): True
+            for k in request.POST if k.startswith(self.PREFIX)
+        }
+
+        if not choices:
+            messages.error(self.request, extra_tags='danger', message="ERROR: You should select at least 1 answer!")
+            return redirect(reverse('tests:testrun_step', kwargs={'pk': pk, 'seq_nr': seq_nr}))
+
+        current_test_result = TestResult.objects.filter(
+            test=test,
+            user=request.user,
+            is_completed=False).last()
+
+        for idx, answer in enumerate(answers, 1):
+            value = choices.get(str(idx), False)
+            TestResultDetail.objects.create(
+                test_result=current_test_result,
+                question=question,
+                answer=answer,
+                is_correct=(value == answer.is_correct)
+            )
+
+        if question.number < test.questions_count():
+            return redirect(reverse('tests:testrun_step', kwargs={'pk': pk, 'seq_nr': seq_nr + 1}))
+        else:
+            current_test_result.finish()
+            current_test_result.save()
+            return render(
+                request=request,
+                template_name='testrun_end.html',
+                context={
+                    'test_result': current_test_result,
+                    'test': test,
+                    'time_spent': datetime.datetime.utcnow() - current_test_result.datetime_run.replace(tzinfo=None)
+                }
+            )
+
+
+class StartTestView(View):
+
+    def get(self, request, pk):
+        qs = TestResult.objects.all()
+        test = Test.objects.get(pk=pk)
+        question_count = test.questions.count()
+        number_runs = test.test_results.count()
+        best_r = qs.aggregate(Max('avr_score')).get('avr_score')
+        print(f'HEEEEEEEEEEEEERE{best_r}')
+        best_user_r = [test.user.get_username()
+                       for test in TestResult.objects.filter(avr_score=best_r)
+                       ]
+        test_result = TestResult.objects.create(
+            user=request.user,
+            test=test
+        )
+
+        return render(
+            request=request,
+            template_name='testrun_start.html',
+            context={
+                'test': test,
+                'test_result': test_result,
+                'question_count': question_count,
+                'number_runs': number_runs,
+                'best_r': best_r,
+                'best_user_r': best_user_r
+            },
+        )
